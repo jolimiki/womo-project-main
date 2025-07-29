b1aff48eeb5b3f14aa350471928fd6e20f77c2ab
jolimiki <prettymiki522@gmail.com>
2025-07-29 17:09:04 +0800

    public web edit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: [
       'images.unsplash.com',
       'd1q14jmvwk39e0.cloudfront.net',
