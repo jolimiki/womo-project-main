c8d03f22fe81c10f24373912476612a6a43ebf0d
jolimiki <prettymiki522@gmail.com>
2025-08-04 16:26:22 +0800

    config set

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isProd ? '/womo-project-main' : '',
+  assetPrefix: isProd ? '/womo-project-main/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
